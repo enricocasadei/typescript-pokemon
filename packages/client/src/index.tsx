ffa91c239c00d2d8295a563a1510540613450e79
enrico <enricocasadei.enrico@gmail.com>
2020-09-25 12:09:22 +0200

    change foldering

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,18 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
