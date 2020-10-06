8eb5f8012f59e738aa65786116bc721f46b2e80b
enrico <enricocasadei.enrico@gmail.com>
2020-10-06 17:04:56 +0200

    complete dnd

--- packages/client/src/component/Favorite/Controller.tsx
+++ packages/client/src/component/Favorite/Controller.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { DragOutlined } from '@ant-design/icons/lib/icons';
 import { useQuery } from '@apollo/client';
-import { Row, Col, List, Typography, Card, Avatar } from 'antd';
+import { Row, Col, Typography, Card, Avatar } from 'antd';
 import { Pokemon, PokemonTableInfo } from '../../type';
 import { GET_POKEMON } from '../../utils/gql';
 import { DragDropContext, Droppable, Draggable, DropResult, DraggableLocation } from 'react-beautiful-dnd';
@@ -50,43 +50,50 @@ export function FavoriteContent(props: { data: Item[] }) {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Row justify="center" align="middle" style={{ textAlign: 'center', height: '100vh' }} gutter={[16, 16]}>
-        {lists.map((list, i) => (
-          <Col key={`list_${i}`} span={12}>
-            <Droppable key={`list_${i}`} droppableId={`list_${i}`}>
-              {(provided, snapshot) => (
-                <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)} {...provided.droppableProps}>
-                  <List
-                    style={{ background: 'white' }}
-                    header={i === 0 ? `List of pokemon` : `List of favorites`}
-                    bordered={true}
-                    itemLayout="vertical"
-                    size="large"
-                    pagination={false}
-                    dataSource={list}
-                    renderItem={(item, index) => (
-                      <Draggable key={item.id} draggableId={item.id} index={index}>
-                        {(provided, snapshot) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-                          >
-                            <PokemonCard pokemon={item} />
-                          </div>
-                        )}
-                      </Draggable>
-                    )}
-                  />
-                </div>
-              )}
-            </Droppable>
-          </Col>
-        ))}
+        {lists.map(DropList)}
       </Row>
     </DragDropContext>
   );
 }
+
+const DropList: (list: Item[], i: number) => React.ReactNode = (list, i) => {
+  const listKey = getUniqueIdFromIndex(i);
+  return (
+    <Col key={listKey} span={12} data-custom={listKey}>
+      <Droppable key={listKey} droppableId={listKey}>
+        {(provided, snapshot) => (
+          <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)} {...provided.droppableProps}>
+            <div style={{ padding: '12px' }}>
+              <Text style={{ fontWeight: 'bold' }}>
+                {i === 0 ? `List of pokemon` : `List of favorites (${list.length} chosen)`}
+              </Text>
+            </div>
+            <div style={{ minHeight: '27956px' }}>{list.map(DragItem(listKey), false)}</div>
+          </div>
+        )}
+      </Droppable>
+    </Col>
+  );
+};
+
+const DragItem: (listKey: 'list' | 'fvrt', disabled?: boolean) => (item: Item, index: number) => React.ReactNode = (
+  listKey,
+  disabled
+) => (item, index) => (
+  <Draggable key={`${listKey}_${item.id}`} draggableId={item.id} index={index} isDragDisabled={disabled}>
+    {(provided, snapshot) => (
+      <div
+        data-custom={`${listKey}_${item.id}`}
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        style={getItemStyle(snapshot.isDragging, provided.draggableProps.style, disabled)}
+      >
+        <PokemonCard pokemon={item} />
+      </div>
+    )}
+  </Draggable>
+);
 
 const mapPokemon = (pokemon: Pokemon): Item => ({
   id: pokemon.node.id,
@@ -118,26 +125,16 @@ const PokemonCard = ({ pokemon }: { pokemon: Item }) => (
 );
 
 const getListStyle = (isDragging: boolean) => ({
-  //background: isDragging ? "lightblue" : "lightgrey",
-  background: isDragging ? '#1890ff' : 'white',
+  background: isDragging ? 'rgba(24, 144, 255,0.4)' : 'white',
 });
 
-const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-  // some basic styles to make the items look a bit nicer
-  userSelect: 'none',
-  padding: 8 * 2,
-  margin: `0 0 ${8}px 0`,
-
-  // change background colour if dragging
-  background: isDragging ? '#1890ff' : 'white',
+const getItemStyle = (isDragging: boolean, draggableStyle: any, disabled?: boolean) => ({
+  padding: 16,
+  background: isDragging ? '#1890ff' : disabled ? '#f5f5f5' : 'white',
   borderRadius: isDragging ? '8px' : undefined,
-  // styles we need to apply on draggables
   ...draggableStyle,
 });
 
-/**
- * Moves an item from one list to another list.
- */
 const move = (
   source: Item[],
   destination: Item[],
@@ -165,4 +162,5 @@ const reorder = (list: Item[], startIndex: number, endIndex: number) => {
   return result;
 };
 
-const getDroppableId = (d: string): number => +d.replace('list_', '');
+const getDroppableId = (d: string): number => (d === 'list' ? 0 : 1);
+const getUniqueIdFromIndex = (i: number): 'list' | 'fvrt' => (i === 0 ? 'list' : 'fvrt');
